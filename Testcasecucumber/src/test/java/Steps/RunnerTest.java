4440befe58a1f54d48052a028899c8aae4dec75c
sam2888 <74895744+sam2888@users.noreply.github.com>
2022-08-07 14:38:18 +0530

    Add files via upload

---
+++ Testcasecucumber/src/test/java/Steps/RunnerTest.java
@@ -0,0 +1,16 @@
+package Steps;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features",glue={"Steps"},
+monochrome=true,
+plugin = { "pretty", "json:target/reports/cucumber.json"}
+)
+
+public class RunnerTest {
+
+}
